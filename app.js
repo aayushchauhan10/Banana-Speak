3e15838b44296abfdc663665640769e380434024
Ayush chauhan <ai.aayush10@gmail.com>
2022-12-26 17:28:58 +0530

    Updating translation API

--- app.js
+++ app.js
@@ -2,10 +2,27 @@ var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#textarea");
 var txtOutput = document.querySelector("#txt-output");
 
+// var serverURL = "https://api.funtranslations.com/translate/yoda.json";
+var serverURL = "https://api.funtranslations.com/translate/minion.json";
+
+function getTranslationURL(text) {
+  return serverURL + "?" + "text=" + text;
+}
+
+function errorHandler(error) {
+  console.log("Error occured: " + error);
+}
+
 function clickEventHandler() {
-  txtOutput.innerText = "This is a test string: " + txtInput.value;
+  var inputText = txtInput.value;
+
+  //calling server for processing
+  fetch(getTranslationURL(inputText))
+    .then((response) => response.json)
+    .then((json) => {
+      console.log(json.contents.translated);
+    })
+    .catch(errorHandler);
 }
 
 btnTranslate.addEventListener("click", clickEventHandler);
-
-//creating APIs
